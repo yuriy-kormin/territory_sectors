5921b138f58c022b98ab79709c685b885c122072
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-12 12:14:34 +0400

    set model flat possibility to set house is NULL

--- territory_sectors/flat/models.py
+++ territory_sectors/flat/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from territory_sectors.house.models import House
+from territory_sectors.language.models import Language
 
 
 # Create your models here.
 class Flat(models.Model):
     date_joined = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    house = models.ForeignKey(to=House, on_delete=models.CASCADE)
+    house = models.ForeignKey(to=House, on_delete=models.SET_NULL, null=True)
     number = models.IntegerField()
     floor = models.IntegerField()
     way_desc = models.CharField(max_length=500)
+    language = models.ForeignKey(to=Language, on_delete=models.SET_DEFAULT, default=1)
