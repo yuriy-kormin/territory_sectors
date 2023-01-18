a43ea6baff4297f2fafee5d09ddef77fb9e43121
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-18 14:06:25 +0400

    add Polygon field to sector model

--- territory_sectors/sector/models.py
+++ territory_sectors/sector/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from territory_sectors.uuid_qr.models import Uuid
-
+from django.contrib.gis.db import models as gis_models
 
 class Sector(models.Model):
     date_joined = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=300, null=True)
     uuid = models.OneToOneField(to=Uuid, null=True, blank=True, on_delete=models.SET_NULL)
-
+    contour = gis_models.PolygonField(null=False, blank=False, srid=4326)
+    # var polygon = {{obj.geom.transform(4326).geojson}};
     def __str__(self):
         return self.name
