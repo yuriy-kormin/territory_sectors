e72b3afe52d60e9b4e300edc48db7e76ffff0f8b
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-24 16:06:37 +0400

    fix gunicorn path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --noinput
 
 # Start server
 echo "Starting jango app"
-gunicorn task_manager.wsgi:application --bind 0.0.0.0:8000
+gunicorn territory_sectors.wsgi:application --bind 0.0.0.0:8000
