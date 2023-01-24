a65057402d62d099b0acec96e011b6b02d67135b
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-24 22:13:04 +0400

    fix staticfiles path

--- territory_sectors/settings.py
+++ territory_sectors/settings.py
@@ -138,7 +138,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(
+        BASE_DIR,
+        'territory_sectors',
+        "static/"),
 )
 
 # Default primary key field type
