d38be1af27798e1bdb16ad1b0b140afbf3fa3317
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-24 21:28:35 +0400

    build nginx

--- Docker/postgis/init.sql
+++ Docker/postgis/init.sql
@@ -1,4 +1,3 @@
 CREATE database sectors;
-REATE user sectors with password 'sectors';
+CREATE user sectors with password 'sectors';
 ALTER database sectors owner to sectors;
-CREATE extension postgis;
