3a5d6e123ef3afb45aa000c86d8fe884021b0862
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-24 21:28:35 +0400

    build nginx

--- Docker/postgis/init.sql
+++ Docker/postgis/init.sql
@@ -1,3 +1,3 @@
-CREATE database sectors;
-CREATE user sectors with password 'sectors';
+CREATE database IF NOT EXISTS sectors;
+CREATE user IF NOT EXISTS sectors with password 'sectors';
 ALTER database sectors owner to sectors;
