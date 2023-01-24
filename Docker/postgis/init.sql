311a197142c77c905ad695f2995438776f88e1b2
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-01-24 22:13:04 +0400

    fix staticfiles path

--- Docker/postgis/init.sql
+++ Docker/postgis/init.sql
@@ -1,3 +1,5 @@
 CREATE database IF NOT EXISTS sectors;
 CREATE user IF NOT EXISTS sectors with password 'sectors';
 ALTER database sectors owner to sectors;
+CREATE EXTENSION postgis;
+ALTER ROLE sectors SUPERUSER;
