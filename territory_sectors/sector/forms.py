973053c03dbb7ba575d580356541f01018ccee6a
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-02-06 15:09:54 +0400

    sector app: create instance: qr list contains only available options

--- territory_sectors/sector/forms.py
+++ territory_sectors/sector/forms.py
@@ -5,7 +5,9 @@ from territory_sectors.uuid_qr.models import Uuid
 
 
 class SectorForm(forms.ModelForm):
+
     class Meta:
+
         model = Sector
         fields = [
             'name',
@@ -19,15 +21,34 @@ class SectorForm(forms.ModelForm):
                     'placeholder': _('Please enter sector name')
                 }
             ),
-            'uuid': forms.Select(
-                attrs={
-                    'class': 'form-control',
-                    'choices': Uuid,
-                }
-            ),
+            # 'uuid': forms.Select(
+            #     queryset=Uuid.objects.filter(
+            #         sector__isnull=True,
+            #         house__isnull=True,
+            #         flat__isnull=True,
+            #     ),
+            #     # attrs={
+            #     #     'class': 'form-control',
+            #     #     'choices': Uuid,
+            #     # }
+            # ),
             'contour': forms.TextInput(
                 attrs={
                     'id': 'contour',
                 },
             ),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(SectorForm, self).__init__(*args, **kwargs)
+        # self.fields['uuid'].queryset =
+        self.fields['uuid'] = forms.ModelChoiceField(
+            queryset=Uuid.objects.filter(
+                    sector__isnull=True,
+                    house__isnull=True,
+                    flat__isnull=True,
+                ),
+            widget=forms.Select(
+                attrs={'class': 'form-control'}
+            )
+        )
