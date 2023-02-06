ff5d6146821504007286b7cf371a3ccbf98f4a2c
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-02-06 13:53:44 +0400

    try to make test for create same flat

--- territory_sectors/tests/flat/test_create.py
+++ territory_sectors/tests/flat/test_create.py
@@ -1,9 +1,30 @@
+import json
+import os
+
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.models import User
+from territory_sectors.flat.models import Flat
 
 
-class Create(TestCase):
+class CreateFlat(TestCase):
+    # fixtures = ['one_flat.json']
+    def setUp(self):
+        # with open(os.path.join('fixtures', 'one_flat.json')) as fixture:
+        #     self.test_task = json.load(fixture)
+        self.user_obj = User.objects.get_or_create(username='testuser')
 
     def test_create_open_without_login(self):
         response = self.client.get(reverse('flat_list'))
         self.assertEqual(response.status_code, 302)
+
+    def test_login_create(self):
+        pass
+
+    def test_create_same(self):
+        # self.assertEqual(User.objects.get(username = 'testuser'), self.user_obj)
+        self.client.force_login(self.user_obj[0])
+        f = Flat.objects.first()
+        self.client.post(path=reverse_lazy('flat_add'),
+                         data=f,
+                         )
