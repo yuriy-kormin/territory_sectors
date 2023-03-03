d6c190dd5d5a2b66c11eecc9a0a5fb28cae7514a
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-03 11:15:53 +0400

    improve print template

--- territory_sectors/settings.py
+++ territory_sectors/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
