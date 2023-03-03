997f750d24b9c7829165d65a4604f9401614581e
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-03 14:09:28 +0400

    backup script

---
+++ bakup_bash_script/backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+set -o allexport
+source ../.env
+set +o allexport
+
+
+# Configuration
+DATABASE_URL="$DATABASE_URL"
+BACKUP_DIR="backups"
+ROTATE_COUNT=30
+DATABASE_CONTAINER_NAME="pgdatabase"
+
+# Parse database connection settings from DATABASE_URL
+export $(python3 -c "import dj_database_url; print('\n'.join(f'DB_{k.upper()}=\"{v}\"' for k, v in dj_database_url.parse('$DATABASE_URL').items()))")
+
+# Create backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
+
+# Get current date and time
+CURRENT_DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+
+# Backup database to file
+docker exec -t $DATABASE_CONTAINER_NAME pg_dump -U $DB_USER -Fc $DB_NAME > $BACKUP_DIR/$CURRENT_DATE.dump
+
+# Rotate backups
+if [ $(ls -1 $BACKUP_DIR/*.dump | wc -l) -gt $ROTATE_COUNT ]; then
+  ls -t $BACKUP_DIR/*.dump | tail -n +$(( $ROTATE_COUNT + 1 )) | xargs rm --
+fi
