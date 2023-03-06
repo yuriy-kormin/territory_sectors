a09e69a975715948b44563ff4ddab977590a5c6b
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-06 14:31:01 +0400

    try to rename ssl config of nginx

--- Docker/certbot/docker-entrypoint.sh
+++ Docker/certbot/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 certbot certonly --agree-tos --keep-until-expiring --webroot -w /var/www/letsencrypt --no-eff-email --email "${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}"
-envsubstr </nginx.conf >/nginx/nginx-ssl.conf
+#envsubstr </nginx.conf >/nginx/nginx-ssl.conf
+mv /nginx/nginx-ssl.tmp /nginx/nginx-ssl.conf
