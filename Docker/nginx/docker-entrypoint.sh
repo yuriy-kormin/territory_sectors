1d2c1d5592226047ad8dfabee761f7386636c668
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-07 11:06:20 +0400

    5

--- Docker/nginx/docker-entrypoint.sh
+++ Docker/nginx/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 rm /etc/nginx/conf.d/default.conf
 cp /app/nginx.conf /etc/nginx/conf.d/nginx.conf
 
-envsubstr </app/nginx-ssl.tmp >/nginx/conf.d/nginx-ssl.tmp
+envsubstr </app/nginx-ssl.tmp >/etc/nginx/conf.d/nginx-ssl.tmp
