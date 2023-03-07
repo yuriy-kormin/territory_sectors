4eb5d0883887f3b32c7442712a563546adc04572
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-07 11:09:36 +0400

    6

--- Docker/nginx/docker-entrypoint.sh
+++ Docker/nginx/docker-entrypoint.sh
@@ -3,4 +3,6 @@
 rm /etc/nginx/conf.d/default.conf
 cp /app/nginx.conf /etc/nginx/conf.d/nginx.conf
 
-envsubstr </app/nginx-ssl.tmp >/etc/nginx/conf.d/nginx-ssl.tmp
+#envsubstr </app/nginx-ssl.tmp >/etc/nginx/conf.d/nginx-ssl.tmp
+sed -i "s/\$CERTBOT_DOMAINS/$CERTBOT_DOMAINS/g" /app/nginx-ssl.tmp
+cp /app/nginx-ssl.tmp /etc/nginx/conf.d/nginx-ssl.tmp
