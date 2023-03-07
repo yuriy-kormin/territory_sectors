035e7cb67d88716dfccf8816b24fa020166dbc7b
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-07 12:08:10 +0400

    14

--- Docker/certbot/docker-entrypoint.sh
+++ Docker/certbot/docker-entrypoint.sh
@@ -1,3 +1,5 @@
 certbot certonly --agree-tos --keep-until-expiring --webroot -w /var/www/letsencrypt --no-eff-email --email "${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}"
 #envsubstr </nginx.conf >/nginx/nginx-ssl.conf
-mv /nginx/nginx-ssl.tmp /nginx/nginx-ssl.conf
+if [ -f /nginx/nginx-ssl.tmp ]; then
+  mv /nginx/nginx-ssl.tmp /nginx/nginx-ssl.conf
+fi
