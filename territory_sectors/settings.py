8519af1e5ecb0ad6f48bb774c559a9f4fa7a306d
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-12 15:30:01 +0400

    REMOVE GDAL

--- territory_sectors/settings.py
+++ territory_sectors/settings.py
@@ -161,9 +161,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-
 INTERNAL_IPS = ('127.0.0.1',)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
