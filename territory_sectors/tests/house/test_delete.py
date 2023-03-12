cf34d57838b08952c1a67ac7f8674a2f523f3cab
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-12 22:04:47 +0400

    add test house model methods

--- territory_sectors/tests/house/test_delete.py
+++ territory_sectors/tests/house/test_delete.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse_lazy
 
+from territory_sectors.flat.models import Flat
 from territory_sectors.house.models import House
 
 
@@ -30,3 +31,21 @@ class Delete(TestCase):
             reverse_lazy('house_delete', kwargs={'pk': house.pk})
         )
         self.assertEqual(House.objects.all().count(), 0)
+
+
+class TestRemoveRelatedFlats(TestCase):
+    fixtures = ['territory_sectors/tests/fixtures/house_with_flats.json']
+
+    def test_related(self):
+        user_obj, _ = User.objects.get_or_create(username='testuser')
+        self.client.force_login(user=user_obj)
+        self.assertEqual(House.objects.all().count(), 1)
+        house = House.objects.first()
+        self.assertEqual(house.flat_set.all().count(), 2)
+        response = self.client.post(
+            reverse_lazy('house_delete', kwargs={'pk': house.pk})
+        )
+        self.assertRedirects(response,
+                             f'{reverse_lazy("house_list")}')
+        self.assertEqual(House.objects.all().count(), 0)
+        self.assertEqual(Flat.objects.all().count(), 0)
