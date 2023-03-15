25c00c834b5c6035c0702270cc5806164c12b2a2
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-15 18:46:53 +0400

    add create superuser on start

--- Docker/sectors/docker-entrypoint.sh
+++ Docker/sectors/docker-entrypoint.sh
@@ -5,15 +5,22 @@
 #### planning to write initadmin  ####
 ######################################
 
-
-# Apply database migrations
-echo "Apply database migrations"
-python /app/manage.py migrate --noinput
-#poetry run python manage.py createsuperuser --noinput
-#collect static
-python /app/manage.py collectstatic --noinput
-
-# Start server
-echo "Starting jango app"
 cd /app/
+echo "Apply database migrations"
+python manage.py migrate --noinput
+
+echo "collect static"
+python manage.py collectstatic --noinput
+
+echo "createsuperuser"
+python manage.py createsuperuser --noinput \
+      --username $DJANGO_SUPERUSER_USERNAME \
+      --email $DJANGO_SUPERUSER_EMAIL
+
+python manage.py -c "from django.contrib.auth.models import User; \
+              user=User.objects.get(username='$DJANGO_SUPERUSER_USERNAME'); \
+              user.set_password('$DJANGO_SUPERUSER_PASSWORD');\
+              user.save();"
+
+echo "Starting jango app"
 gunicorn territory_sectors.wsgi:application --bind 0.0.0.0:8000
