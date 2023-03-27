82afda145698f188b931be596ff31ca3a5695409
Yuriy Kormin <yuriy.kormin@gmail.com>
2023-03-27 08:29:23 +0400

    fix sector draw rectangle on sector_change page

--- app/territory_sectors/language/admin.py
+++ app/territory_sectors/language/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Language
 from simple_history.admin import SimpleHistoryAdmin
-# Register your models here.
 
 admin.site.register(Language, SimpleHistoryAdmin)
