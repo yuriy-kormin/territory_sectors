7a13ed9d827f0a20f38023eb0fe72bb7c26e9ec4
yuriy.kormin <yuriy.kormin@gmail.com>
2023-05-01 22:09:52 +0400

    use external font file

--- app/territory_sectors/sector/viewPDFprint.py
+++ app/territory_sectors/sector/viewPDFprint.py
@@ -1,10 +1,20 @@
+from django.conf import settings
 from django.views.generic import View
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.pdfbase import pdfmetrics
 from .models import Sector
 from .PdfGen.process_response import make_response
+import os
 
-pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
+font_path = os.path.join(
+    settings.BASE_DIR,
+    'territory_sectors',
+    'sector',
+    'PdfGen',
+    'font',
+    'arial.ttf'
+)
+pdfmetrics.registerFont(TTFont('Arial', font_path))
 # pdfmetrics.registerFont(TTFont('TimesCyr', 'times.ttf'))
 
 class SectorPrintPDF(View):
