bad82987c38139fe72f6ff049a8db675dcf1e46a
yuriy.kormin <yuriy.kormin@gmail.com>
2023-05-17 18:22:48 +0400

    migration fix 3

--- Docker/postgis/init.sql
+++ Docker/postgis/init.sql
@@ -1,5 +1,5 @@
-CREATE database IF NOT EXISTS sectors;
-CREATE user IF NOT EXISTS sectors with password 'sectors';
+CREATE database sectors;
+CREATE user sectors with password 'sectors';
 ALTER database sectors owner to sectors;
 CREATE EXTENSION postgis;
 ALTER ROLE sectors SUPERUSER;
