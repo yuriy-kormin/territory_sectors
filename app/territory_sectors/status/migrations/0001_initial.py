dc4d05fbae4716319ca7b9ed2b77823021d453cd
yuriy.kormin <yuriy.kormin@gmail.com>
2023-05-17 17:10:33 +0400

    add status instance to firdt migration

--- app/territory_sectors/status/migrations/0001_initial.py
+++ app/territory_sectors/status/migrations/0001_initial.py
@@ -4,10 +4,14 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 import simple_history.models
+from ..models import Status
+
+
+def create_status_instance(apps, schema_editor):
+    Status.objects.create(name='free')
 
 
 class Migration(migrations.Migration):
-
     initial = True
 
     dependencies = [
@@ -18,20 +22,30 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Status',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, unique=True)),
             ],
         ),
         migrations.CreateModel(
             name='HistoricalStatus',
             fields=[
-                ('id', models.BigIntegerField(auto_created=True, blank=True, db_index=True, verbose_name='ID')),
+                ('id', models.BigIntegerField(auto_created=True, blank=True,
+                                              db_index=True,
+                                              verbose_name='ID')),
                 ('name', models.CharField(db_index=True, max_length=200)),
-                ('history_id', models.AutoField(primary_key=True, serialize=False)),
+                ('history_id',
+                 models.AutoField(primary_key=True, serialize=False)),
                 ('history_date', models.DateTimeField(db_index=True)),
-                ('history_change_reason', models.CharField(max_length=100, null=True)),
-                ('history_type', models.CharField(choices=[('+', 'Created'), ('~', 'Changed'), ('-', 'Deleted')], max_length=1)),
-                ('history_user', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to=settings.AUTH_USER_MODEL)),
+                ('history_change_reason',
+                 models.CharField(max_length=100, null=True)),
+                ('history_type', models.CharField(
+                    choices=[('+', 'Created'), ('~', 'Changed'),
+                             ('-', 'Deleted')], max_length=1)),
+                ('history_user', models.ForeignKey(null=True,
+                                                   on_delete=django.db.models.deletion.SET_NULL,
+                                                   related_name='+',
+                                                   to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'verbose_name': 'historical status',
@@ -41,4 +55,5 @@ class Migration(migrations.Migration):
             },
             bases=(simple_history.models.HistoricalChanges, models.Model),
         ),
+        migrations.RunPython(create_status_instance),
     ]
