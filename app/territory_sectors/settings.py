025f33d513a8061ba679bfaf5bb904705e1cb96c
yuriy.kormin <yuriy.kormin@gmail.com>
2023-05-24 23:05:00 +0400

    remove dublicates in static files

--- app/territory_sectors/settings.py
+++ app/territory_sectors/settings.py
@@ -159,12 +159,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(
-        BASE_DIR,
-        'territory_sectors',
-        "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(
+#         BASE_DIR,
+#         'territory_sectors',
+#         "static"),
+# )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
