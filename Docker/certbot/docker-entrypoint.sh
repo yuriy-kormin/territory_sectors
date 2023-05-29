a082a24d3607582281b0c434a6ae8823283c15f9
yuriy.kormin <yuriy.kormin@gmail.com>
2023-05-29 20:36:19 +0400

    move ideas from django-certbot-nginx project

--- Docker/certbot/docker-entrypoint.sh
+++ Docker/certbot/docker-entrypoint.sh
@@ -1,6 +1,42 @@
-certbot certonly --agree-tos --keep-until-expiring --webroot -w /var/www/letsencrypt --no-eff-email --email "${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}"
-#envsubstr </nginx.conf >/nginx/nginx-ssl.conf
-#if [ -f /nginx/nginx-ssl.tmp ]; then
-#  mv /nginx/nginx-ssl.tmp /nginx/nginx-ssl.conf
-#  rm /nginx/nginx.conf
-#fi
+certbot certonly --agree-tos --keep-until-expiring \
+--webroot -w /var/www/letsencrypt --no-eff-email \
+--email "${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}" \
+#--dry-run
+
+cert_dir="/etc/letsencrypt/live/${CERTBOT_DOMAINS}"
+
+NGINX_CONF_DIR="/nginx/"
+
+if [ -z "$CERTBOT_DOMAINS" ]; then
+    echo "ERROR: $CERTBOT_DOMAINS environment variable is not set."
+    exit 1
+fi
+#check if cert exist
+if [ -d "${cert_dir}" ]; then
+  echo "cert dir exists - cert received or exist"
+
+  if [ -f "${NGINX_CONF_DIR}nginx-ssl.tmp" ]; then
+    echo "-----  exchanging nginx to nginx.ssl  -----"
+
+    if [ -e "${NGINX_CONF_DIR}nginx-ssl.conf" ]; then
+      rm "${NGINX_CONF_DIR}nginx-ssl.conf"
+      echo "nginx-ssl.conf  removed before applying a new one"
+    fi
+
+    echo "replacing DOMAIN NAME from .env "
+    sed -i "s|\$CERTBOT_DOMAINS|${CERTBOT_DOMAINS}|g" "${NGINX_CONF_DIR}nginx-ssl.tmp"
+
+    echo "   renaming nginx-ssl.tmp to nginx-ssl.conf"
+    mv "${NGINX_CONF_DIR}nginx-ssl.tmp" "${NGINX_CONF_DIR}nginx-ssl.conf"
+
+    echo "   removing old conf"
+    rm "${NGINX_CONF_DIR}nginx.conf"
+  else
+    echo "ssl conf exist. skip"
+
+  fi
+
+else
+  echo "ERROR Cert dir doesnot exists - cert not received"
+  exit 1
+fi
--- Docker/nginx/docker-entrypoint.sh
+++ Docker/nginx/docker-entrypoint.sh
@@ -4,11 +4,6 @@ set -e
 if [ -f /etc/nginx/conf.d/default.conf ]; then
     rm /etc/nginx/conf.d/default.conf
 fi
-cp /app/nginx.conf /etc/nginx/conf.d/nginx.conf
-
-#envsubstr </app/nginx-ssl.tmp >/etc/nginx/conf.d/nginx-ssl.tmp
-sed -i "s/\$CERTBOT_DOMAINS/$CERTBOT_DOMAINS/g" /app/nginx-ssl.tmp
-cp /app/nginx-ssl.tmp /etc/nginx/conf.d/nginx-ssl.tmp
 
 #making log dir with files
 nginx_log_dir="/var/log/nginx"
@@ -20,23 +15,39 @@ if [ ! -d "$nginx_log_dir" ]; then
   # Create the directory
   sudo mkdir -p "$nginx_log_dir"
   echo "Created directory $nginx_log_dir"
+  # Create access log file with root:root ownership
+  sudo touch "$nginx_log_dir/$access_log_file"
+  sudo chown root:root "$nginx_log_dir/$access_log_file"
+  echo "Created $access_log_file with root:root ownership"
+
+  # Create error log file with root:root ownership
+  sudo touch "$nginx_log_dir/$error_log_file"
+  sudo chown root:root "$nginx_log_dir/$error_log_file"
+  echo "Created $error_log_file with root:root ownership"
 else
   echo "Directory $nginx_log_dir already exists"
 fi
 
-# Create access log file with root:root ownership
-sudo touch "$nginx_log_dir/$access_log_file"
-sudo chown root:root "$nginx_log_dir/$access_log_file"
-echo "Created $access_log_file with root:root ownership"
+### copy config files if necessary
+NGINX_TEMPLATE_DIR="/etc/nginx/conf.templates/"
+NGINX_CONF_DIR="/etc/nginx/conf.d/"
 
-# Create error log file with root:root ownership
-sudo touch "$nginx_log_dir/$error_log_file"
-sudo chown root:root "$nginx_log_dir/$error_log_file"
-echo "Created $error_log_file with root:root ownership"
+if [ -f "${NGINX_CONF_DIR}nginx-ssl.conf" ]; then
+  echo "ssl config exists. skip configuration"
+else
+  echo "ssl config file not found. start configuration"
 
-# Define a function to run the command you want to run when the file changes
+  rm -f "${NGINX_CONF_DIR}/*"
+  echo "removing all files from ${NGINX_CONF_DIR}"
+
+  cp "${NGINX_TEMPLATE_DIR}"/* "${NGINX_CONF_DIR}"
+  echo "copy config from templates"
+
+fi
+##start daemon
+service nginx start
+
 function run_command {
-  # Replace this with the command you want to run
   echo "Config file changed! Reloading nginx..."
   service nginx reload
 }
@@ -44,7 +55,3 @@ function run_command {
 # Use inotifywait to monitor the directory for changes
 inotifywait -m /etc/nginx/conf.d/ -e move |
     run_command
-  done &
-
-#CMD ["sh", "-c", "while inotifywait -e modify,create,delete /etc/nginx/conf.d/; do nginx -s reload; done & nginx -g \"daemon off;\""]
-nginx -g "daemon off;"
