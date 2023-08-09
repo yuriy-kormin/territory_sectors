482a3400ce37f8a1b41e576974636addca1013e6
= <=>
2023-08-10 03:15:06 +0400

    separate frontend image

--- Docker/react/docker-entrypoint.sh
+++ Docker/react/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "certboot domains = ${CERTBOT_DOMAINS}"
+
 if [ -n "$CERTBOT_DOMAINS" ]; then
   DOMAIN=$(echo "$CERTBOT_DOMAINS" | cut -d',' -f1)
   REACT_APP_BACKEND_URL="https://${DOMAIN}/graphql/"
@@ -8,3 +8,11 @@ if [ -n "$CERTBOT_DOMAINS" ]; then
 else
   echo "CERTBOT_DOMAINS environment variable is not set. REACT_APP_BACKEND_URL will use default."
 fi
+
+cd /app/
+echo "npm run build"
+npm build
+
+echo "Starting react app"
+
+npm start
