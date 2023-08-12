9de140b079c86c24b8bbc6404f6aca52d8992f2b
= <=>
2023-08-12 09:24:39 +0400

    backup script 4

--- backup_bash_script/backup.sh
+++ backup_bash_script/backup.sh
@@ -11,7 +11,7 @@ CURRENT_DATE=$(date +"%Y-%m-%d")
 
 # Configuration
 BACKUP_DIR="backups"
-FOLDER_TO_BACKUP="app/territory_sectors/app/media"
+FOLDER_TO_BACKUP="territory_sectors/app/media"
 ROTATE_COUNT=30
 DATABASE_CONTAINER_NAME="pgdatabase"
 
