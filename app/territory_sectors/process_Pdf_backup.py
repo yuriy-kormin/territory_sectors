f58d5055cf657e444b359ba2736c692ef1ae1885
yuriy.kormin <yuriy.kormin@gmail.com>
2023-08-14 09:33:52 +0000

    add backup script to backup to cloud

--- app/territory_sectors/process_Pdf_backup.py
+++ app/territory_sectors/process_Pdf_backup.py
@@ -19,39 +19,25 @@ sys.path.insert(0, PROJECT_ROOT)
 from django.test import RequestFactory
 from django.conf import settings
 from django.apps import apps
-from datetime import datetime
 
+settings.ALLOWED_HOSTS.append('testserver')
 apps.populate(settings.INSTALLED_APPS)
+backup_folder = "/backup/"
 
 from territory_sectors.sector.PdfGen.utils import set_font
-# from reportlab.pdfbase import pdfmetrics
-# from reportlab.pdfbase.ttfonts import TTFont
 from territory_sectors.sector.PdfGen.process_response import make_response
-#
-
-
-
-
 from territory_sectors.sector.models import Sector
-settings.ALLOWED_HOSTS.append('testserver')
-
-# client = Client()
-BACKUP_FOLDER = "."
 
 set_font()
 request_factory = RequestFactory()
-queryset = Sector.objects.all()[:1]
+queryset = Sector.objects.all()[:2]
 
 for sector in queryset:
-    # request = client.get('/')
     request = request_factory.get('/')
     pdf_response = make_response(request, sector)
     if pdf_response \
             and pdf_response.status_code == 200 \
             and pdf_response['Content-Type'] == 'application/pdf':
-        now = datetime.now()
-        date_folder = now.strftime("%Y-%m-%d")
-        backup_folder = os.path.join(BACKUP_FOLDER, date_folder)
 
         if not os.path.exists(backup_folder):
             os.makedirs(backup_folder)
@@ -62,4 +48,4 @@ for sector in queryset:
         with open(pdf_path, 'wb') as pdf_file:
             pdf_file.write(pdf_response.content)
 
-        print(f"Generated and saved PDF backup for instance {sector.name}.")
+        print(f"Generated and saved PDF backup for sector {sector.name}.")
--- backup_bash_script/backup.sh
+++ backup_bash_script/backup.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+## To correctly run this script, you need to install several tools
+##
+## jq  #pasrse json data
+## zip
+##  rclone (to upload backups to clouds)
+##
+
+# Configuration
+BACKUP_DIR="backups"
+FOLDER_TO_BACKUP="territory_sectors/app/media"
+FOLDER_WITH_PDFS="territory_sectors/backup"
+ROTATE_COUNT=30
+DATABASE_CONTAINER_NAME="pgdatabase"
+DJANGO_CONTAINER_NAME="sectors-sectors"
+REMOTE_NAME="my"  # The name of the remote configured in rclone
+REMOTE_STORE_FOLDER="backup_sectors"  # The name of the remote folder in rclone
+REMOTE_ROTATE_COUNT=3 # Count of the remote backups stored
+
 # Get the absolute path of the directory that contains the script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Load environment variables from .env file
@@ -9,26 +27,64 @@ set +o allexport
 # Get current date and time
 CURRENT_DATE=$(date +"%Y-%m-%d")
 
-# Configuration
-BACKUP_DIR="backups"
-FOLDER_TO_BACKUP="territory_sectors/app/media"
-ROTATE_COUNT=30
-DATABASE_CONTAINER_NAME="pgdatabase"
-
-
 # Create backup directory if it doesn't exist
 mkdir -p $BACKUP_DIR/$CURRENT_DATE
-zip -r "$BACKUP_DIR/$CURRENT_DATE/app" "$FOLDER_TO_BACKUP"
+zip -r "$BACKUP_DIR/$CURRENT_DATE/media" "$FOLDER_TO_BACKUP"
 
 POSTGRES_CONTAINER_ID=$(docker container ls -q -f name=$DATABASE_CONTAINER_NAME)
+DJANGO_CONTAINER_ID=$(docker container ls -q -f name=$DJANGO_CONTAINER_NAME)
 
-echo "$POSTGRES_USER"
-echo "$POSTGRES_DB"
 # Backup database to filecd ..
 docker exec -t $POSTGRES_CONTAINER_ID pg_dump -U $POSTGRES_USER -d $POSTGRES_DB > $BACKUP_DIR/$CURRENT_DATE/$CURRENT_DATE.dump
+
+#backup pdfs with sectors
+docker exec $DJANGO_CONTAINER_ID bash -c "export DJANGO_SETTINGS_MODULE='territory_sectors.settings' && cd /app && python3 territory_sectors/process_Pdf_backup.py"
+
+# zip result
+zip -r "$BACKUP_DIR/$CURRENT_DATE/pdf" "$FOLDER_WITH_PDFS"
 
 # Rotate backups
 if [ $(ls -1 $BACKUP_DIR/* | wc -l) -gt $ROTATE_COUNT ]; then
   ls -t $BACKUP_DIR/* | tail -n +$(( $ROTATE_COUNT + 1 ))
   ls -t $BACKUP_DIR/* | tail -n +$(( $ROTATE_COUNT + 1 )) | xargs rm -r --
 fi
+
+####################################
+####   UPLOAD TO REMOTE CLOUD   ####
+####################################
+echo "upload to remote"
+rclone copy $BACKUP_DIR/$CURRENT_DATE $REMOTE_NAME:$REMOTE_STORE_FOLDER/$CURRENT_DATE
+
+
+# Retrieve the remote folder contents and store it in a JSON file
+rclone lsjson $REMOTE_NAME:$REMOTE_STORE_FOLDER > remote_contents.json
+
+# Extract folder names (dates) and modification times from the JSON file
+folders_and_dates=$(jq -r '.[] | select(.IsDir) | "\(.Name) \(.ModTime)"' remote_contents.json)
+
+# Sort folders by date (modification time)
+sorted_folders_and_dates=$(echo "$folders_and_dates" | sort -k 2 -r)
+
+# Extract the folder names from the sorted list
+sorted_folders=$(echo "$sorted_folders_and_dates" | cut -d ' ' -f 1)
+
+# Calculate the number of folders to keep
+keep_count=$(($(echo "$sorted_folders" | wc -l) - $REMOTE_ROTATE_COUNT))
+
+# Extract the folders to delete
+folders_to_delete=$(echo "$sorted_folders" | tail -n $keep_count)
+
+# Loop through and delete folders
+for folder in $folders_to_delete; do
+    echo "remove ${REMOTE_NAME}:${REMOTE_STORE_FOLDER}/${folder}"
+    rclone purge $REMOTE_NAME:$REMOTE_STORE_FOLDER/$folder
+done
+
+# Remove the temporary JSON file
+rm remote_contents.json
+
+
+
+
+
+
