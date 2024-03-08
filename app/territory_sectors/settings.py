55ada0a34761a6b730a7e3fdb400e27e1ec03edd
Yuriy Kormin <yuriy.kormin@gmail.com>
2024-03-08 18:35:21 +0400

    add db_port in config

--- app/territory_sectors/settings.py
+++ app/territory_sectors/settings.py
@@ -130,6 +130,7 @@ db_conf = {
     'username': os.environ.get("POSTGRES_USER"),
     'password': os.environ.get("POSTGRES_PASSWORD"),
     'db_name': os.environ.get("POSTGRES_DB"),
+    'db_port': os.environ.get("POSTGRES_PORT", 5432),
 }
 docker_db_host = os.environ.get("DOCKER_DB_HOST", '')
 db_conf['hostname'] = docker_db_host \
@@ -137,7 +138,7 @@ db_conf['hostname'] = docker_db_host \
 
 if all(db_conf.values()):
     database_url = f"postgis://{db_conf['username']}:{db_conf['password']}@" \
-                   f"{db_conf['hostname']}:5432/{db_conf['db_name']}"
+                   f"{db_conf['hostname']}:{db_conf['db_port']}/{db_conf['db_name']}"
 else:
     database_url = 'spatialite:///db.sqlite3'
 os.environ['DATABASE_URL'] = database_url
