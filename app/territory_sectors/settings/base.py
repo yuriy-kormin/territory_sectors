e0cc899840d0538f307d1399b71208848b996523
Yuriy Kormin <yuriy.kormin@gmail.com>
2024-03-30 15:47:34 +0400

    fast fix if not exist image

--- app/territory_sectors/settings/base.py
+++ app/territory_sectors/settings/base.py
@@ -5,7 +5,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv()
+env_path = os.path.join(BASE_DIR.parent.parent, '.env-compose')
+load_dotenv(env_path)
 
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
@@ -41,7 +42,6 @@ WSGI_APPLICATION = 'territory_sectors.wsgi.application'
 
 GOOGLE_ANALYTICS_KEY = os.environ.get("GOOGLE_ANALYTICS_KEY")
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -61,7 +61,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
