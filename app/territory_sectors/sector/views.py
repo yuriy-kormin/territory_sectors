2a317e9e7579facb9e8723d6866d62e5a0bd1333
Yuriy Kormin <yuriy.kormin@gmail.com>
2024-05-09 23:56:02 +0400

    add  manual

--- app/territory_sectors/sector/views.py
+++ app/territory_sectors/sector/views.py
@@ -51,32 +51,6 @@ class SectorUpdateView(LoginRequiredMixinCustom, CentroidAnnotateMixin,
     }
     success_message = _('Sector updated successfully')
 
-    # def get_context_data(self, **kwargs):
-    #     """Add context."""
-    #     context = super().get_context_data(**kwargs)
-    #     url = self.request.build_absolute_uri(
-    #         self.object.get_absolute_url(
-    #             reverse("uuid", kwargs={'pk': self.object.uuid_id})
-    #         )
-    #     )
-    #     img = make(url)
-    #
-    #     buffer = BytesIO()
-    #     img.save(buffer)
-    #     buffer.seek(0)
-    #
-    #     file_size = len(buffer.getvalue())
-    #     file_name = f"qr_{self.id}.png"
-    #     file_content = ContentFile(buffer.read(), name=file_name)
-    #     img_data = base64.b64encode(
-    #         InMemoryUploadedFile(
-    #             file_content, None, file_name, 'image/png', file_size, None
-    #         ).read()
-    #     ).decode('utf-8')
-    #
-    #     context['qr_img_data'] = img_data
-    #     return context
-
 
 class SectorDeptorsListView(LoginRequiredMixinCustom, AddDebtorsMixin,
                             ListView):
