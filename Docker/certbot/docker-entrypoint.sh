6dfdd01cce3cc7c943db65d65704f49b5ee7ea9b
Iurii Kormin <iurii.kormin@aspose.com>
2026-08-03 17:58:35 +0400

    remove --keep-until-removal key in certbot

--- Docker/certbot/docker-entrypoint.sh
+++ Docker/certbot/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-certbot certonly --agree-tos --keep-until-renewal \
+certbot certonly --agree-tos  \
 --webroot -w /var/www/letsencrypt --no-eff-email \
 --email "${CERTBOT_EMAIL}" -d "${CERTBOT_DOMAINS}" \
 #--dry-run
